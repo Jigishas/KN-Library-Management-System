12d17eb9b0c5b8f2be6d60792850a6a0f028a173
Jigisha336 <jigishagachuru336@gmail.com>
2026-03-15 16:20:04 +0300

    few updates

--- Db.php
+++ Db.php
@@ -8,6 +8,6 @@ $conn=new mysqli($server, $user, $password, $dbname);
 if(!$conn){
     echo "Connection failed: " . mysqli_connect_error();
 } else {
-    echo "Connected successfully";
+    echo "Connected successfully"."<br>";
 }
 ?>
--- Register.php
+++ Register.php
@@ -1,4 +1,22 @@
+<?php 
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    include 'Db.php';
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $role = $_POST['role'];
+
+    $sql = "INSERT INTO users (name, email, password, role) VALUES ('$name', '$email', '$password', '$role')";
+    if(mysqli_query($conn, $sql)){
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+?>
 <!DOCTYPE html>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -10,12 +28,14 @@
     <form action="Register.php" method="post">
         <label for="name">Name</label>
         <input type="text" name="name" placeholder="Name">
+        <br>
         <label for="email">Email</label>
-
         <input type="email" name="email" placeholder="Email">
+        <br>
         <label for="password">Password</label>
         <input type="password" name="password" placeholder="Password">
-        <input type="text" name="" value="User" hidden>
+        <br>
+        <input type="text" name="role" value="User" hidden>
         <button type="submit">Register</button>
 
     </form>

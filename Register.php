e383fbed9e3b99de9e0fc0e643315e2bf58b8c9a
Jigisha336 <jigishagachuru336@gmail.com>
2026-03-15 16:29:23 +0300

    few updates

--- Register.php
+++ Register.php
@@ -15,14 +15,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 }
 
 ?>
-<!DOCTYPE html>
 
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <body>
     <div>
     <form action="Register.php" method="post">

9800c21001d3ee2721f397dba0862c82d325ce21
Jigisha336 <jigishagachuru336@gmail.com>
2026-03-15 15:35:45 +0300

    first commit

---
+++ Db.php
@@ -0,0 +1,13 @@
+<?php 
+$server="localhost";
+$user="root";
+$password="";
+$dbname="Librarydb";
+
+$conn=new mysqli($server, $user, $password, $dbname);
+if(!$conn){
+    echo "Connection failed: " . mysqli_connect_error();
+} else {
+    echo "Connected successfully";
+}
+?>
